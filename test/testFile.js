37e9c6ee836f8a97b27c87aa16aa38b03d1e7137
harshita808 <harshitasaxenah7@gmail.com>
2022-08-13 17:12:38 +0530

    test

--- test/testFile.js
+++ test/testFile.js
@@ -1,13 +1,12 @@
+// add your test suite or use a template above
 const Decentralized=artifacts.require('Decentralized');
+const truffleAssert = require('truffle-assertions');
 
 contract('Decentralized',(accounts) => {
-
-    it('Should deploy smart contract properly',async () => {
-         const dec=await Decentralized.deployed();
-         assert(dec.address !== '');
-         
-         
-    });
+  beforeEach(async () => {
+    
+});   
+   
 
 
     it('should transfer ether to another address', async() => {
@@ -26,4 +25,22 @@ contract('Decentralized',(accounts) => {
   
           
         });
+
+        it("Sending amount less than 1eth should fail", async () => {
+          // check if send function is working for normal case
+         // const demo = await Wallet.deployed();
+         const dec=await Decentralized.deployed();
+          await truffleAssert.passes(
+             dec.send(accounts[1],{value: web3.utils.toWei('2', 'ether')}),
+             'Send function should be implemented correctly.'
+          )
+          // now check if it has require statement for amount less than 1 ether
+          await truffleAssert.reverts(
+             dec.send(accounts[1],{value: web3.utils.toWei('0.9', 'ether')}),
+          )
+          // now check if it can send 1 ether
+          await truffleAssert.passes(
+             dec.send(accounts[1], {value:web3.utils.toWei('1', 'ether')}),
+          )
+       });
 });       
