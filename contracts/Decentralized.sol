4eb767359bbe7705baeb179f67c58ac344438923
harshita <64593306+harshita808@users.noreply.github.com>
2022-08-13 17:20:40 +0530

    update

--- contracts/Decentralized.sol
+++ contracts/Decentralized.sol
@@ -3,10 +3,5 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract Decentralized {
 
-    uint public amount = 1 ether;
-
-    function send(address payable _addr) payable public {
-        require(msg.value >= amount);
-        _addr.transfer(msg.value);
-    }
+    //Write your code here
 }
